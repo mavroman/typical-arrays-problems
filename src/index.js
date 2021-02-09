af8d5410c0b22787def6e81184256805f65a0721
Roman Mavrin <roma-mrv@yandex.ru>
2021-02-09 17:45:06 +0300

    docs: update arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) return []
+  else {
+    array.sort((a,b) => a - b)
+    return array[0]
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0) return []
+  else {
+      array.sort((a,b) => b - a)
+  return array[0]
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) return []
+  else return (array.reduce((a,b) => (a + b)))/array.length
 }
